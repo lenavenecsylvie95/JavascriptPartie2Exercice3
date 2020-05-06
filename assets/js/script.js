ef86d56dd52078ff56001c33c8578a34c165fd70
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-05-06 22:32:43 +0200

    Corrigé

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+function extend(){
+  // je stock dans ma variable imgWidth la valeur de la largeur de mon image
+  var imgWidth = document.getElementById('logo').clientWidth;
+  console.log(imgWidth);
+  // j'attribue une nouvelle valeur à la width de mon image en ajoutant 20px à partir de la taille récupérée dans imgWidth
+  document.getElementById('logo').style.width = (imgWidth + 150) + 'px';
+}
